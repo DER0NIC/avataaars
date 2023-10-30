8a8a3724eefec3c2c6c924e495b9d3951d748212
DERONIC <96531649+DER0NIC@users.noreply.github.com>
2023-10-30 12:44:51 +0100

    Update color_extension.dart

--- lib/helper/color_extension.dart
+++ lib/helper/color_extension.dart
@@ -3,13 +3,17 @@ import 'package:flutter/material.dart';
 extension HexColor on Color {
   /// String is in the format "aabbcc" or "ffaabbcc" with an optional leading "#".
   static Color fromHex(String hexString) {
-    final buffer = StringBuffer();
-    if (hexString.length == 6 || hexString.length == 7) buffer.write('ff');
-    buffer.write(hexString.replaceFirst('#', ''));
-    return Color(int.parse(buffer.toString(), radix: 16));
+    hexString = hexString.replace("#", "");
+    if (hexString.length == 6) {
+      return Color(int.parse("FF$hexString", radix: 16));
+    }
+    // length is hopefully 8
+    return Color(int.parse(hexString, radix: 16));
   }
 
   /// Prefixes a hash sign if [leadingHashSign] is set to `true` (default is `true`).
-  String toHex({bool leadingHashSign = true}) => '${leadingHashSign ? '#' : ''}'
-      '${value.toRadixString(16).toUpperCase()}';
+  String toHex({bool leadingHashSign = true}) {
+    String str = '${leadingHashSign ? '#' : ''}';
+    str = str + '${value.substring(2).padLeft(6, '0').toRadixString(16).toUpperCase()}';
+  }
 }
